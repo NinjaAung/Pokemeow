63d0724ce26f0062bfd074c57294c37941e514bf
NinjaAung <ninja.aung02@gmail.com>
2021-01-08 17:10:33 -0800

    Objective 1
    Program Logins and prompts a test message

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ driver.get("https://discord.com/channels/719694038088810506/740628046284980390")
 
 username = config["USER_NAME"]
 password = config["USER_PASSWORD"]
+message = "***The following is a test!***"
+pokemeow = ";clan members"
 
 sleep(2)
 
@@ -33,3 +35,22 @@ sleep(1)
 
 keyboard.press(Key.enter)
 keyboard.release(Key.enter)
+
+sleep(5)
+
+for letter in message:
+    keyboard.press(letter)
+    keyboard.release(letter)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+sleep(1)
+
+for letter in pokemeow:
+    keyboard.press(letter)
+    keyboard.release(letter)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
