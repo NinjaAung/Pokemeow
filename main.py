4ba70c31452aa53d67824378b6db93ecf6ac9aff
NinjaAung <ninja.aung02@gmail.com>
2021-01-08 17:02:22 -0800

    Login has been made

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from pynput.keyboard import Key, Controller
 from selenium import webdriver
-from dotenv import load_dotenv
-import time
+from dotenv import dotenv_values
+from time import sleep
 import csv
 import re
-load_dotenv()
+
+keyboard = Controller()
+config = dotenv_values(".env")
+
+driver = webdriver.Chrome("./drivers/chromedriver")
+driver.get("https://discord.com/channels/719694038088810506/740628046284980390")
+
+username = config["USER_NAME"]
+password = config["USER_PASSWORD"]
+
+sleep(2)
+
+for letter in username:
+    keyboard.press(letter)
+    keyboard.release(letter)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+sleep(1)
+
+for letter in password:
+    keyboard.press(letter)
+    keyboard.release(letter)
+
+sleep(1)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
