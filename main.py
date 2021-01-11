4935e6c2675357bdb8a62c1d003414cb445274b6
NinjaAung <ninja.aung02@gmail.com>
2021-01-11 00:54:36 -0800

    DRY

--- main.py
+++ main.py
@@ -7,78 +7,55 @@ from time                           import sleep
 import gspread
 import re
 
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/spreadsheets']
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json",scope)
+# Google Creds
+scope  = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/spreadsheets']
+creds  = ServiceAccountCredentials.from_json_keyfile_name("creds.json",scope)
 client = gspread.authorize(creds)
-
 sheet = client.open_by_key("1hgHs8gCUOWnUsCBMw122XTSQeLCzRMa1C1FXT_OusnI").sheet1
 
+# Config
+config   = dotenv_values(".env")
+USERNAME = config["USER_NAME"]
+PASSWORD = config["USER_PASSWORD"]
+
+# Messages
+start_message   = "Starting Count!"
+pokemeow        = ";clan members"
+pokemeow_next   = "next"
+pruning         = "t@prune 5"
+
+# Vaiables
+curr_members              = []
+curr_members_join_date    = []
+curr_members_contribution = []
+
+driver   = webdriver.Chrome("./drivers/chromedriver")
 keyboard = Controller()
-config = dotenv_values(".env")
-
-members                 = []
-members_join_date       = []
-members_contribution    = []
-
-driver = webdriver.Chrome("./drivers/chromedriver")
-
-# Random Corp Server
-# driver.get("https://discord.com/channels/719694038088810506/797292197811585066")
-
-# Testing
-driver.get("https://discord.com/channels/750761968067018859/797230922393976866")
-
-username = config["USER_NAME"]
-password = config["USER_PASSWORD"]
-start_message = "Starting Count!"
-pokemeow = ";clan members"
-pokemeow_next = "next"
-pruning = "t@prune 5"
+# driver.get("https://discord.com/channels/719694038088810506/797292197811585066") # Random Corp Server
+driver.get("https://discord.com/channels/750761968067018859/797230922393976866") # Testing
+keyboard = Controller()
 
 def sanitazing(members,contrib):
     for i in range(0,len(members)):
         members[i]  = members[i][3:].strip()
         contrib[i]  = re.sub(r"\|.*","",contrib[i]).strip()
 
+def typing(message,start_in=0,start_next=0):
+    sleep(start_in)
+    for letter in message:
+        keyboard.press(letter)
+        keyboard.release(letter)
 
-sleep(2)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    sleep(start_next)
 
-for letter in username:
-    keyboard.press(letter)
-    keyboard.release(letter)
 
-keyboard.press(Key.enter)
-keyboard.release(Key.enter)
+typing(USERNAME,2)
+typing(PASSWORD,2)
+typing(start_message,6)
+typing(pokemeow,1)
 
-sleep(1)
-
-for letter in password:
-    keyboard.press(letter)
-    keyboard.release(letter)
-
-sleep(1)
-
-keyboard.press(Key.enter)
-keyboard.release(Key.enter)
-
-sleep(6)
-for letter in start_message:
-    keyboard.press(letter)
-    keyboard.release(letter)
-
-keyboard.press(Key.enter)
-keyboard.release(Key.enter)
-
-sleep(1)
-
-for letter in pokemeow:
-    keyboard.press(letter)
-    keyboard.release(letter)
-
-keyboard.press(Key.enter)
-keyboard.release(Key.enter)
-
-sleep(2)
 
 for i in range(0,4):
     sleep(2)
@@ -92,62 +69,46 @@ for i in range(0,4):
         date_joined     = driver.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div/main/div[1]/div/div/div/div[4]/div[2]/div/div/div[4]/div[3]/div[2]").text.splitlines()
         
     sanitazing(member,contribution)
-    members                 += member
-    members_join_date       += date_joined 
-    members_contribution    += contribution
+
+    curr_members                 += member
+    curr_members_join_date       += date_joined 
+    curr_members_contribution    += contribution
+
+    typing(1,pokemeow_next)
+
+typing(1,2)
+
+curr_member_cells_list       = sheet.range('H2:H51')
+curr_contribution_cells_list = sheet.range('I2:I51')
+curr_days                    = sheet.range('J2:J51')
+past_members_list            = sheet.range('F2:F51')
 
 
-
-    for letter in pokemeow_next:
-        keyboard.press(letter)
-        keyboard.release(letter)
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
-
-for letter in pruning:
-    keyboard.press(letter)
-    keyboard.release(letter)
-
-keyboard.press(Key.enter)
-keyboard.release(Key.enter)
-
-
-new_member_cells_list       = sheet.range('H2:H51')
-new_contribution_cells_list = sheet.range('I2:I51')
-new_days                    = sheet.range('J2:J51')
-
-
-
-for i in range(0,len(members)):
-    timedelta = datetime.now() - datetime.fromisoformat(members_join_date[i])
-    if members[i] == "":
-        members[i] = ":fries:"
+for i in range(0,len(curr_members)):
+    timedelta = datetime.now() - datetime.fromisoformat(curr_members_join_date[i])
+    if curr_members[i] == "":
+        curr_members[i] = ":fries:"
     if timedelta.days <= 0:
-        avg  = 0
-        days = 0
-        
+        days = 1
     else:
-        avg  = int(members_contribution[i].replace(",","")) / int(timedelta.days)
         days = timedelta.days
 
-    new_member_cells_list[i].value          = members[i]
-    new_contribution_cells_list[i].value    = members_contribution[i]
-    new_days[i].value                       = days
+    curr_member_cells_list[i].value          = curr_members[i]
+    curr_contribution_cells_list[i].value    = curr_members_contribution[i]
+    curr_days[i].value                       = days
 
-#TODO: if user dosen't  exsist create a new row
 
-sheet.update_cells(new_member_cells_list)
-print("Members Added ✅")
-sheet.update_cells(new_contribution_cells_list)
-print("Contrinutions Added ✅")
-sheet.update_cells(new_days)
-print("Stay Added ✅")
+# sheet.update_cells(curr_member_cells_list)
+# print("Members Added ✅")
+# sheet.update_cells(curr_contribution_cells_list)
+# print("Contributions Added ✅")
+# sheet.update_cells(curr_days)
+# print("Stay Added ✅")
 
 #TODO: If a member leaves delete that row then format
 
-past_members_list = sheet.range('C2:C51')
 for i in range(len(past_members_list)):
-    if not past_members_list[i].value in members:
+    if not past_members_list[i].value in curr_members:
         if past_members_list[i].value == "":
             break
         print(past_members_list[i].value)
@@ -155,6 +116,17 @@ for i in range(len(past_members_list)):
         sheet.update_cell(past_members_list[i].row,2,"")
         sheet.update_cell(past_members_list[i].row,3,"")
         sheet.update_cell(past_members_list[i].row,6,"1")
+
+#TODO: IF a new member appears append to the list
+
+for i in range(len(curr_members)):
+    try:
+        sheet.find(curr_members[i])
+    except:
+        sheet.update_cell(past_members_list[49].row,1,"0")
+        sheet.update_cell(past_members_list[49].row,2,curr_members_contribution[i])
+        sheet.update_cell(past_members_list[49].row,3,curr_members[i])
+        sheet.update_cell(past_members_list[49].row,6,curr_days[i])
 
 sleep(2)
 
